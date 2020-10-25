272dbbd55d902f1d3fb1b7bc7628dbd059526c87
xizhizeze <xizhizeze@163.com>
2020-10-25 16:01:13 +0800

    code commit

---
+++ 4-6.c
@@ -0,0 +1,21 @@
+#include"ch03.h"
+
+
+
+int main()
+{
+	
+	int d1;
+	d1=open("./out.info",O_CREAT|O_TRUNC|O_WRONLY,0644);
+	//dup2(d1,1);
+	//d1=dup(STDOUT_FILENO);
+	
+	
+	printf("Redirect stdout file!\nn");
+	
+	return 0;
+
+
+}
+
+
