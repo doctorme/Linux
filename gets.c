650305849c79129725dcbcdc74510037e2be36a6
xizhizeze <xizhizeze@163.com>
2020-10-26 14:45:45 +0800

    code commit

---
+++ gets.c
@@ -0,0 +1,9 @@
+#include"ch03.h"
+int main()
+{
+char s[10];
+printf("put in:\n");
+gets(s);
+printf("%s\n",s);
+return 0;
+}
---
+++ w-6.c
@@ -0,0 +1,17 @@
+#include"ch03.h"
+int main()
+{
+printf("max int =%d\n",INT_MAX);
+printf("max long =%ld\n",LONG_MAX);
+printf("max char =%c\n",CHAR_MAX);
+printf("max float =%f\n",FLT_MAX);
+printf("max double =%lf\n",DBL_MAX);
+
+printf("min int =%d\n",INT_MIN);
+printf("min long =%ld\n",LONG_MIN);
+printf("min char =%c\n",CHAR_MIN);
+printf("min float =%f\n",FLT_MIN);
+printf("min double =%lf\n",DBL_MIN);
+
+return 0;
+}
