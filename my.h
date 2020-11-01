ed6370ce437a5307b6b713373a0780ca9333a98c
xizhizeze <xizhizeze@163.com>
2020-11-01 22:07:17 +0800

    code commit

---
+++ my.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+
+
+typedef struct user{
+	unsigned int id;
+	char name[9];
+	char htel[13];
+	char tel[13];
+}record;
+void input(void);
+void save(record*,const char*);
+void mysort(void);
+void randdat(void);
+void output(void);
