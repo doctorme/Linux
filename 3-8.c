ea4d0b03e8116a1a420f5820f9c3aab47c2f38a4
xizhizeze <xizhizeze@163.com>
2020-11-02 10:35:48 +0800

    delete

--- 3-8.c
+++
@@ -1,9 +0,0 @@
-#include "ch3.h"
-
-int main(){
-	FILE *fp;
-	fp=fdopen(1,"w+");//1是已经打开的文件的文件描述符，后面的是文件的的打开方式
-	fprintf(fp,"test data %s\n","nihao");
-
-	return 0;
-}
--- gets.c
+++
@@ -1,9 +0,0 @@
-#include"ch03.h"
-int main()
-{
-char s[10];
-printf("put in:\n");
-gets(s);
-printf("%s\n",s);
-return 0;
-}
